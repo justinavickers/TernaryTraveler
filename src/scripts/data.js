4ec7496d8939dd95e31eefec782d6505902fbc27
justinavickers <justinav38@gmail.com>
2019-02-22 16:05:48 -0600

    fixed a name

--- src/scripts/data.js
+++ src/scripts/data.js
@@ -3,7 +3,7 @@ import makePointOfInterest from "./interestComponent";
 const article = document.querySelector(".output");
 
 function renderAllInterest(interest) {
-  journalEntries.forEach(interest => {
+  interest.forEach(interest => {
     article.innerHTML += makePointOfInterest(interest);
   });
 }
